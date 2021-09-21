124a488874ee30f3e2986f958b47b1f94622afe1
Ruslan Osmanov <rrosmanov@gmail.com>
2021-09-21 17:06:00 +0300

    Fixed -Wformat-overflow errors for PHP 8

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+old_CFLAGS="$CFLAGS"
+# Note, -Wformat-overflow values > 0 only trigger errors when optimization mode (-O) is turned on (> 0).
+export CFLAGS="$CFLAGS -g -O2 -Wformat-overflow=2"
+
 phpize
-./configure --with-eio
+./configure --with-eio "$@"
 make
+
+export CFLAGS="$old_CFLAGS"
--- rebuild.sh
+++ rebuild.sh
@@ -2,6 +2,13 @@
 phpize --clean
 phpize
 #aclocal && libtoolize --force && autoreconf
-./configure --with-eio --enable-eio-debug
+
+old_CFLAGS="$CFLAGS"
+# Note, -Wformat-overflow values > 0 only trigger errors when optimization mode (-O) is turned on (> 0).
+export CFLAGS="$CFLAGS -g -O2 -Wformat-overflow=2"
+
+./configure --with-eio "$@"
 make clean
 make -j3
+
+export CFLAGS="$old_CFLAGS"
